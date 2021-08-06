4ad5a654feda6810f0512e7861ba285c08ea1ca8
Mazhar Hussain <mmazharhussainkgb1145@gmail.com>
2021-08-06 21:34:16 +0500

    fish_prompts.fish: there's an empty line after each prompt

--- fish_prompts.fish
+++ fish_prompts.fish
@@ -4,6 +4,6 @@ for prompt in /usr/share/fish/tools/web_config/sample_prompts/*
   echo -n "$n. "; basename $prompt | sed 's/.fish$//g'
   source $prompt
   fish_prompt
-  echo
+  echo -e '\n'
   set n (math $n+1)
 end
