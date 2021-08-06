e4f290e5e6955ad95f1386d5a3c66a86c5bc3deb
Mazhar Hussain <mmazharhussainkgb1145@gmail.com>
2021-08-06 21:31:14 +0500

    fish_prompts.fish: now name shows up before the prompt is shown

--- fish_prompts.fish
+++ fish_prompts.fish
@@ -1,6 +1,9 @@
 #!/bin/fish
+set n 1
 for prompt in /usr/share/fish/tools/web_config/sample_prompts/*
+  echo -n "$n. "; basename $prompt | sed 's/.fish$//g'
   source $prompt
   fish_prompt
-  basename $prompt | sed 's/.fish$//g'
+  echo
+  set n (math $n+1)
 end
