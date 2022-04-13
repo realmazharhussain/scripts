fe97ec71b1f37432affc533a1d6433be50043856
Mazhar Hussain <realmazharhussain@gmail.com>
2022-04-13 18:56:13 +0500

    rewrite clean-with-gitignore.sh with better logic
    basically, now, it doesn't try to rm -rf /

--- clean-with-gitignore.sh
+++ clean-with-gitignore.sh
@@ -1,3 +1,18 @@
 #!/bin/bash
-list=($(<.gitignore))
-rm -rf ${list[@]}
+if test ! -f .gitignore;
+then
+   echo "'.gitignore' file not found" >&2
+   exit 1
+fi
+while read line;
+do
+   line=$(echo "$line" | sed -E 's/(#.*)|(^[[:space:]]*$)|^\///g')
+   eval files=($line)
+   if [[ "$line" != '' ]] && [[ "${files[*]}" != *\** ]]
+   then
+      for file in "${files[@]}"
+      do
+         rm -rf "$file"
+      done
+   fi
+done < .gitignore
