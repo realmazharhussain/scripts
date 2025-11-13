682dd33e94e2b0d35c0b7b528cb7ceba269a705e
Mazhar Hussain <realmazharhussain@gmail.com>
2025-11-13 10:08:42 +0500

    battery-health.sh: Support different types of batteries

--- battery-health.sh
+++ battery-health.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 source <(sed 's/ /\\ /' /sys/class/power_supply/BAT0/uevent)
-echo $((100*POWER_SUPPLY_CHARGE_FULL/POWER_SUPPLY_CHARGE_FULL_DESIGN))%
+if [[ -n $POWER_SUPPLY_CHARGE_FULL_DESIGN ]]; then
+  echo $((100*POWER_SUPPLY_CHARGE_FULL/POWER_SUPPLY_CHARGE_FULL_DESIGN))%
+elif [[ -n $POWER_SUPPLY_ENERGY_FULL_DESIGN ]]; then
+  echo $((100*POWER_SUPPLY_ENERGY_FULL/POWER_SUPPLY_ENERGY_FULL_DESIGN))%
+else
+  echo "Couldn't figure out battery health!"
+fi
