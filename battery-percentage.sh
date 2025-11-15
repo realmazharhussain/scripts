a06152760ab93eed6040757314a83e588b68483f
Mazhar Hussain <realmazharhussain@gmail.com>
2025-11-15 21:59:54 +0500

    battery-percentage.sh: Support different types of batteries

--- battery-percentage.sh
+++ battery-percentage.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 source <(sed 's/ /\\ /' /sys/class/power_supply/BAT0/uevent)
-echo $((100*POWER_SUPPLY_CHARGE_NOW/POWER_SUPPLY_CHARGE_FULL))%
+
+if [[ -n $POWER_SUPPLY_CHARGE_FULL ]]; then
+  echo $((100*POWER_SUPPLY_CHARGE_NOW/POWER_SUPPLY_CHARGE_FULL))%
+elif [[ -n $POWER_SUPPLY_ENERGY_FULL_DESIGN ]]; then
+  echo $((100*POWER_SUPPLY_ENERGY_NOW/POWER_SUPPLY_ENERGY_FULL))%
+else
+  echo "Couldn't figure out battery percentage!"
+fi
